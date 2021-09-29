ad3d0b2834f1363f36c1aa7e270475b17eb15a6b
corvinus-aoad9f <peter.tolvaj@stud.uni-corvinus.hu>
2021-09-29 21:12:47 +0200

    bindinglist és gomb funkcioja

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +21,17 @@ namespace UserMaintenance
             lblLastName.Text = Resource1.LastName;
             btnAdd.Text = Resource1.Add;
 
+            listUsers.DataSource = listUsers;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = txtLastName.Text,
+                FirstName = txtFirstName.Text
+            };
+            users.Add(u);
         }
     }
 }
