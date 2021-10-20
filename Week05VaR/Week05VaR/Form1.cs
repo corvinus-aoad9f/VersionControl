d11778a98bd3e0d180054cb54727fdc2890e1749
corvinus-aoad9f <peter.tolvaj@stud.uni-corvinus.hu>
2021-10-21 01:05:00 +0200

    ötödik feladatig

--- Week05VaR/Week05VaR/Form1.cs
+++ Week05VaR/Week05VaR/Form1.cs
@@ -12,9 +12,24 @@ namespace Week05VaR
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+        List<Entities.PortfolioItem> Portfolio = new List<Entities.PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new Entities.PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new Entities.PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new Entities.PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
