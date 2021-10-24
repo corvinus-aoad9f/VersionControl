6f07542d94f75a8d0daa4772756b8719bd9530bf
corvinus-aoad9f <peter.tolvaj@stud.uni-corvinus.hu>
2021-10-24 17:26:25 +0200

    dinamic functions

--- Week06/Week06/Form1.cs
+++ Week06/Week06/Form1.cs
@@ -21,6 +21,13 @@ namespace Week06
         public Form1()
         {
             InitializeComponent();
+            RefreshData();
+            
+        }
+
+        private void RefreshData()
+        {
+            Rates.Clear();
             Webszolghiv();
             dataGridView1.DataSource = Rates;
             XMLData();
@@ -68,12 +75,27 @@ namespace Week06
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox1.SelectedItem.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString(),
             };
             var response = mnbService.GetExchangeRates(request);
             result = response.GetExchangeRatesResult;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
