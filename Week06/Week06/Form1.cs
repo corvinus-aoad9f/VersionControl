9a3a5a522e0a08a05cb81cdb481343600f5b8848
corvinus-aoad9f <peter.tolvaj@stud.uni-corvinus.hu>
2021-10-24 17:06:19 +0200

    xml feldolgozasa

--- Week06/Week06/Form1.cs
+++ Week06/Week06/Form1.cs
@@ -9,17 +9,38 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Week06.MnbServiceReference;
 using Week06.Entities;
+using System.Xml;
 
 namespace Week06
 {
     public partial class Form1 : Form
     {
         BindingList<RateData> Rates = new BindingList<RateData>();
+        public string result;
         public Form1()
         {
             InitializeComponent();
             Webszolghiv();
             dataGridView1.DataSource = Rates;
+            XMLData();
+        }
+
+        private void XMLData()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
 
         private void Webszolghiv()
@@ -32,7 +53,7 @@ namespace Week06
                 endDate = "2020-06-30"
             };
             var response = mnbService.GetExchangeRates(request);
-            var result = response.GetExchangeRatesResult;
+            result = response.GetExchangeRatesResult;
         }
     }
 }
