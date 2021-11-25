692bf210ada139c7ce1c4e3886e66d08627db20a
corvinus-aoad9f <peter.tolvaj@stud.uni-corvinus.hu>
2021-11-25 22:19:18 +0100

    teszt probalkozas

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using UnitTestExample.Controllers;
@@ -10,7 +11,11 @@ namespace UnitTestExample.Test
 {
     public class AccountControllerTestFixture
     {
-        [Test]
+        [Test,
+        TestCase("abcd1234", false),
+        TestCase("irf@uni-corvinus", false),
+        TestCase("irf.uni-corvinus.hu", false),
+        TestCase("irf@uni-corvinus.hu", true)]
         public void TestValidateEmail(string email, bool expectedResult)
         {
             // Arrange
@@ -21,6 +26,22 @@ namespace UnitTestExample.Test
 
             // Assert
             Assert.AreEqual(expectedResult, actualResult);
+            
         }
+        [Test,
+            TestCase("Kiskutya"),
+            TestCase("K1skutya"),
+            TestCase("K1skuty"),
+            TestCase("k1skutya"),
+            TestCase("K1SKUTYA")]
+        public bool TestValidatePassword(string password)
+        {
+            /*var Length = new Regex(@".{8,}");
+            var LowerCase = new Regex(@"[a-z]+");
+            var UpperCase = new Regex(@"[A-Z]+");
+            var Number = new Regex(@"[0-9]+");*/
+            return /*Length.IsMatch(password)&&LowerCase.IsMatch(password)&&UpperCase.IsMatch(password)&&Number.IsMatch(password)*/true;
+        }
+        
     }
 }
