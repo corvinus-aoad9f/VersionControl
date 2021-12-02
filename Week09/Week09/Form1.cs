62f862dbcf4db22c05882fadaf819411514246bd
corvinus-aoad9f <peter.tolvaj@stud.uni-corvinus.hu>
2021-12-02 17:35:02 +0100

    adatokbetoltse

--- Week09/Week09/Form1.cs
+++ Week09/Week09/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Week09.Entities;
+using System.IO;
 
 namespace Week09
 {
@@ -18,7 +19,74 @@ namespace Week09
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
         public Form1()
         {
+            Population = ReadPerson(@"d:\Users\Peter\Downloads\nép-teszt.csv");
+            BirthProbabilities = ReadBirth(@"d:\Users\Peter\Downloads\születés.csv");
+            DeathProbabilities = ReadDeath(@"d:\Users\Peter\Downloads\halál.csv");
             InitializeComponent();
+        }
+
+        public List<DeathProbability> ReadDeath(string csvpath)
+        {
+            List<DeathProbability> death = new List<DeathProbability>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    death.Add(new DeathProbability()
+                    {
+                        BirthYear = int.Parse(line[0]),
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                        P = double.Parse(line[2])
+                    });
+                }
+            }
+
+            return death;
+
+        }
+
+        public List<BirthProbability> ReadBirth(string csvpath)
+        {
+            List<BirthProbability> birth = new List<BirthProbability>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    birth.Add(new BirthProbability()
+                    {
+                        BirthYear = int.Parse(line[0]),
+                        NbrOfChildren = int.Parse(line[1]),
+                        P = double.Parse(line[2])
+                    });
+                }
+            }
+
+            return birth;
+        }
+
+        public List<Person> ReadPerson(string csvpath)
+        {
+            List<Person> population = new List<Person>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    population.Add(new Person()
+                    {
+                        BirthYear = int.Parse(line[0]),
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                        NbrOfChildren = int.Parse(line[2])
+                    });
+                }
+            }
+
+            return population;
         }
     }
 }
